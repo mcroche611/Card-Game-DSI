6cab07582a41d4ce5c4f0dadbe326c3aa057fd06
mcroche611 <mcroche@ucm.es>
2022-04-21 12:49:32 +0200

    Cartas del tablero de juego

--- card_game_DSI/Board.xaml.cs
+++ card_game_DSI/Board.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+using System.Collections.ObjectModel;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,10 +20,22 @@ namespace card_game_DSI
     /// </summary>
     public sealed partial class Board : Page
     {
+
+        public ObservableCollection<Card> boardCards { get; } = new ObservableCollection<Card>();
+        public ObservableCollection<Card> card1 { get; } = new ObservableCollection<Card>();
+        public ObservableCollection<Card> card2 { get; } = new ObservableCollection<Card>();
+        public ObservableCollection<Card> card3 { get; } = new ObservableCollection<Card>();
+        public ObservableCollection<Card> card4 { get; } = new ObservableCollection<Card>();
+        public ObservableCollection<Card> handCards { get; } = new ObservableCollection<Card>();
+
+        int Num = -1;
+        Card PickedCard;
+
         public Board()
         {
             this.InitializeComponent();
         }
+
         private void BackButton_OnClick(object sender, RoutedEventArgs e)
         {
             // First, check that it's safe to ask the Frame to go backward.
@@ -34,6 +44,81 @@ namespace card_game_DSI
                 // If there's a page in the "backstack," we can call GoBack().
                 Frame.GoBack();
             }
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Cosntruye las listas de ModelView a partir de la lista Modelo 
+            if (boardCards != null)
+                foreach (Card card in BoardCards.GetBoardCards())
+                {
+                    boardCards.Add(card);
+                }
+
+            if (handCards != null)
+                foreach (Card card in BoardCards.GetHandCards())
+                {
+                    handCards.Add(card);
+                }
+
+            base.OnNavigatedTo(e);
+
+            this.Loaded += delegate { this.Focus(FocusState.Programmatic); };
+        }
+
+        private void BasicGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+
+        }
+
+        private void Card_DragOver(object sender, DragEventArgs e)
+        {
+            e.AcceptedOperation = DataPackageOperation.Copy;
+        }
+
+        private void Card1_Drop(object sender, DragEventArgs e)
+        {
+            if (card1.Count < 1 && PickedCard != null)
+            {
+                card1.Add(PickedCard);
+                handCards.Remove(PickedCard);
+                PickedCard = null;
+            }
+        }
+
+        private void Card2_Drop(object sender, DragEventArgs e)
+        {
+            if (card2.Count < 1 && PickedCard != null)
+            {
+                card2.Add(PickedCard);
+                handCards.Remove(PickedCard);
+                PickedCard = null;
+            }
+        }
+
+        private void Card3_Drop(object sender, DragEventArgs e)
+        {
+            if (card3.Count < 1 && PickedCard != null)
+            {
+                card3.Add(PickedCard);
+                handCards.Remove(PickedCard);
+                PickedCard = null;
+            }
+        }
+
+        private void Card4_Drop(object sender, DragEventArgs e)
+        {
+            if (card4.Count < 1 && PickedCard != null)
+            {
+                card4.Add(PickedCard);
+                handCards.Remove(PickedCard);
+                PickedCard = null;
+            }
+        }
+
+        private void HandGridView_DragItemsStarting(object sender, DragItemsStartingEventArgs e)
+        {
+            Card Item = e.Items[0] as Card;
+            PickedCard = Item;
         }
     }
 }
---
+++ card_game_DSI/BoardCards.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace card_game_DSI
+{
+    public class BoardCards
+    {
+        public static List<Card> boardCards = new List<Card>()
+        {
+            new Card("Rey Rata", "Rey Rata", 6123),
+            new Card("Rana Toro", "Rana Toro", 6120, "This will be an interesting research"),
+            new Card("Sabueso", "BubbleYuno", 6114, "You seem a little dirty, don't ya?"),
+            new Card("Vibora", "JudyVertigo", 6111, "Help me!"),
+        };
+
+        public static List<Card> boardCards2 = new List<Card>()
+        {
+             new Card("Vibora", "JudyVertigo", 6111, "Help me!"),
+            new Card("Rey Rata", "Kaycee21", 6123),
+        };
+
+        public static List<Card> handCards = new List<Card>()
+        {
+             new Card("Vibora", "JudyVertigo", 6111, "Help me!", 0),
+            new Card("Rey Rata", "Kaycee21", 6123, "hello", 1),
+            new Card("Rana Toro", "Rana Toro", 6120, "This will be an interesting research", 2),
+            new Card("Sabueso", "BubbleYuno", 6114, "You seem a little dirty, don't ya?", 3),
+        };
+
+        public static IList<Card> GetBoardCards()
+        {
+            return boardCards;
+        }
+
+        public static IList<Card> GetBoardCards2()
+        {
+            return boardCards2;
+        }
+
+        public static IList<Card> GetHandCards()
+        {
+            return handCards;
+        }
+
+        public static Card GetBoardCardById(int id)
+        {
+            return boardCards[id];
+        }
+
+        public static Card GetBoardCard2ById(int id)
+        {
+            return boardCards2[id];
+        }
+
+        public static Card GetHandCardById(int id)
+        {
+            return handCards[id];
+        }
+    }
+}
+
