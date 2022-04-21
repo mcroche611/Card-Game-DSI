7aa8805bc20b15f18de7f4b2af5ea6047be7dfd5
elenrobeUCM <elenrobe@ucm.es>
2022-04-21 12:58:02 +0200

    Revert "mi mazo"
    This reverts commit 98396c232241e3c3a9a766417040c89d426679e0.

--- card_game_DSI/MyDeck.xaml.cs
+++ card_game_DSI/MyDeck.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -23,10 +22,6 @@ namespace card_game_DSI
     /// </summary>
     public sealed partial class MyDeck : Page
     {
-
-        public ObservableCollection<Card> allCards { get; } = new ObservableCollection<Card>();
-        public ObservableCollection<Card> myDeck { get; } = new ObservableCollection<Card>();
-
         public MyDeck()
         {
             this.InitializeComponent();
@@ -40,30 +35,5 @@ namespace card_game_DSI
                 Frame.GoBack();
             }
         }
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            // Cosntruye las listas de ModelView a partir de la lista Modelo 
-            if (allCards != null)
-                foreach (Card card in CardCollection.GetCards())
-                {
-                    allCards.Add(card);
-                }
-            base.OnNavigatedTo(e);
-
-            this.Loaded += delegate { this.Focus(FocusState.Programmatic); };
-        }
-
-        private void BasicGridView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            myDeck.Add(e.ClickedItem as Card);
-
-        }
-
-        private void BasicGridView2_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            myDeck.Remove(e.ClickedItem as Card);
-
-        }
-
     }
 }
