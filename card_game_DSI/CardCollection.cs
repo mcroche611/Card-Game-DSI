f35d3d05a5b3c7782e65bd242af30383314e6453
elenrobeUCM <elenrobe@ucm.es>
2022-05-03 11:41:16 +0200

    updated cardCollection para que tenga mas cartas

--- card_game_DSI/CardCollection.cs
+++ card_game_DSI/CardCollection.cs
@@ -10,17 +10,24 @@ namespace card_game_DSI
     {
         public static List<Card> cards = new List<Card>()
         {
-            new Card("Rey Rata", "Rey Rata"), 
-            new Card("Rana Toro", "Rana Toro", "This will be an interesting research"), 
-            new Card("Sabueso", "BubbleYuno", "You seem a little dirty, don't ya?"), 
-            new Card("Vibora", "JudyVertigo", "Help me!"),
-            new Card("Vibora", "JudyVertigo", "Help me!"),
-            new Card("Rey Rata", "Kaycee21"),
-            new Card("Rana Toro", "Rana Toro", "This will be an interesting research"),
-            new Card("Sabueso", "BubbleYuno", "You seem a little dirty, don't ya?"),
-            new Card("Vibora", "JudyVertigo", "Help me!"),
-            new Card("Vibora", "JudyVertigo", "Help me!"),
-            new Card("Zarigueya", "RodriguezPaellas", "Paella va!") 
+            new Card("Abeja", "Abeja", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Alce Macho", "Alce Macho", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Alce", "Alce", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Alfa", "Alfa", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Amalgama", "Amalgama", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Ardilla", "Ardilla", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Berrendo", "Berrendo", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Cuervo", "Cuervo", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Gato", "Gato", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Grizzly", "Grizzly", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Mantis", "Mantis", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Coyote", "Coyote", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Conejo", "Conejo", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Geco", "Geco", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Cabra Negra", "Cabra Negra", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Hurraca", "Hurraca", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Rana Toro", "Rana Toro", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio."),
+            new Card("Topo", "Topo", "tiene una cualidad de vuelo, con 1 de ataque y un coste de 1 punto de sacrificio.")
         };
 
         public static IList<Card> GetCards()
